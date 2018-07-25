852bdfc67a43fd9782b07a76f84e0a1b20fd902c
Albie <albion@dragonfruit.ml>
2018-07-25 21:40:10 +0200

    Update install_server_reverseproxy.sh

--- installer-src/install_server_reverseproxy.sh
+++ installer-src/install_server_reverseproxy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-printf Welcome to the jel.ly installer
-printf Do you want a reverse proxy installed with a working config? y/N
+echo -e Welcome to the jel.ly installer
+echo -e Do you want a reverse proxy installed with a working config? y/N
 read varname
-printf \n\nInstalling dependencies...\n\n
+echo -e \n\nInstalling dependencies...\n\n
 apt update
 apt install -y screen
 apt install -y npm
@@ -19,18 +19,18 @@ rm /etc/nginx/conf.d/default.conf
 rm /etc/nginx/sites-enabled/default
 wget https://aspriddell.github.io/jel.ly/installer-src/default.conf -P /etc/nginx/conf.d
 nginx -s reload
-printf Do you want to enable SSL For Free, Provided by Let's Encrypt? y/N
+echo -e Do you want to enable SSL For Free, Provided by Let's Encrypt? y/N
 read varname1
 if [ $varname1 = "y" ];
 then
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt update
 sudo apt install -y python-certbot-nginx
-printf \n\n\nThe Let's Encrypt Configurator will now load. Please fill out the details correctly
+echo -e \n\n\nThe Let's Encrypt Configurator will now load. Please fill out the details correctly
 certbot --nginx
 fi
 fi
-printf \n\nInstalling jelly server\n
+echo -e \n\nInstalling jelly server\n
 cd
 wget https://aspriddell.github.io/jel.ly/installer-src/startserver.sh
 git clone https://github.com/aspriddell/jel.ly.git
@@ -38,8 +38,8 @@ cd jel.ly
 npm install -g gulp
 npm install
 
-printf "\n\nSetup Complete. The module screen has been installed. If you are planing on having an always on server run:"
-printf "screen -t jelly"
-printf "sudo bash startserver.sh"
-printf "\n\nIf you configured Let's Encrypt. Run sudo crontab -e and paste:\n0 0 1 * * certbot renew\nat the bottom"
+echo -e "\n\nSetup Complete. The module screen has been installed. If you are planing on having an always on server run:"
+echo -e "screen -t jelly"
+echo -e "sudo bash startserver.sh"
+echo -e "\n\nIf you configured Let's Encrypt. Run sudo crontab -e and paste:\n0 0 1 * * certbot renew\nat the bottom"
 
