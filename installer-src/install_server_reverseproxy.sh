f05ed4001de8df1c31ac01adc2975de494488f3f
Albie <albion@dragonfruit.ml>
2018-07-25 08:54:05 +0200

    Replace install_server_reverseproxy.sh

--- installer-src/install_server_reverseproxy.sh
+++ installer-src/install_server_reverseproxy.sh
@@ -1,29 +1,27 @@
-if [ "$EUID" -ne 0 ]
-  then echo "je.ly Installer: Please run as root (sudo)"
-  exit
-fi
-
-
-echo "jel.ly installer (with reverse proxy)"
-read -p "Do you want to install nginx and a working config file? y/N" nginxacc
-
-echo "Installing Dependencies & Upgrading System"
-apt update && apt upgrade
-apt install -y screen npm nodejs git
-
-if ["$nginxacc"=="y"] || ["$nginxacc"=="Y"] ; then
-apt install nginx
-sudo systemctl start nginx
-sudo systemctl enable nginx
+#!/bin/bash
+# Ask the user for their name
+echo Welcome to the jel.ly installer
+echo Do you want to have a reverse proxy installed with a working config?
+read varname
+echo Installing dependencies
+apt update
+apt install screen
+apt install npm
+apt install nodejs
+apt install git
+if [ $varname="y" ];
+then
+apt-get install nginx
+systemctl start nginx
+systemctl enable nginx
 rm /etc/nginx/conf.d/default.conf
-
 wget https://aspriddell.github.io/jel.ly/installer-src/default.conf -P /etc/nginx/conf.d
 nginx -s reload
 fi
 
-cd 
+cd
 wget https://aspriddell.github.io/jel.ly/installer-src/startserver.sh
-git clone https://github.com/aspriddell/jel.ly.git
+git clone git clone https://github.com/aspriddell/jel.ly.git
 cd jel.ly
 npm install -g gulp
 npm install
@@ -32,3 +30,4 @@ echo "Setup Complete. The modue screen has been installed. If you are planing on
 echo "Run screen -t jelly"
 echo "Then at your home dir: sudo bash startserver.sh"
 echo "Have Fun!"
+
